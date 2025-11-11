176f39fa5050f6cda6fba9dd7be6b13944d5a55b
lucaldev <lucaldev22@gmail.com>
2025-11-11 20:07:32 -0300

    shortest job first

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
 import Processo from './processo.js';
-
 import RoundRobin from './roundRobin.js';
+import ShortestJobFirst from "./shortestJobFirst.js";
+
 
 const processos = [
-  new Processo(1, 't1', 5000),
-  new Processo(2, 't2', 2000),
+  new Processo(1, 't1', 0, 5000, 3),
+  new Processo(2, 't2', 0, 2000, 2),
+] 
+
+const processosAdicionais = [
+  new Processo(2, 't3',  1000,4000, 5),
+   new Processo(3, 't4', 3000, 1000, 2),
+    new Processo(4, 't5', 5000, 2000, 1),
 ]
 
+const shortestJobFirst = new ShortestJobFirst();
+
+processos.forEach(processo => shortestJobFirst.adicionarProcesso(processo));
+
+shortestJobFirst.executar();
+
+processosAdicionais.forEach(p => setTimeout(() => {
+  shortestJobFirst.adicionarProcesso(p);
+}, p.getIngresso()));
+
+
+
+/* --ROUND ROBIN--
 const roundRobin = new RoundRobin(2000);
 
 processos.forEach(processo => roundRobin.adicionarProcesso(processo));
 
 roundRobin.executar();
 
-setTimeout(() => {
-  roundRobin.adicionarProcesso(new Processo(3, 't3', 4000));
-}, 1000);
+processosAdicionais.forEach(p => setTimeout(() => {
+  roundRobin.adicionarProcesso(p);
+}, p.getIngresso()));
 
-setTimeout(() => {
-  roundRobin.adicionarProcesso(new Processo(4, 't4', 1000));
-}, 2000);
-
-setTimeout(() => {
-  roundRobin.adicionarProcesso(new Processo(5, 't5', 2000));
-}, 3000);
+*/
---
+++ shortestJobFirst.js
@@ -0,0 +1,43 @@
+class ShortestJobFirst {
+  constructor() {
+    this.fila = [];
+    this.espera = 0;
+    this.execucao = 0;
+    this.nProcessos = 0;
+    this.duracao = 0;
+    this.tempoTotal;
+  }
+
+  adicionarProcesso(processo) {
+    console.log(`##### Adicionando processo ${processo.getPid()} à fila #####`);
+    this.fila.push(processo);
+    this.nProcessos++;
+  }
+
+  async executar() {
+    while (this.fila.length > 0) {
+      // Organiza a fila de processos
+      this.fila.sort((a, b) => a.getDuracao() - b.getDuracao());      
+
+      const processoAtual = this.fila.shift();
+      const filaPids = this.fila.map(p => p.getNome());
+      console.log('\n------------------------------');
+      console.log(`Fila atual de processos (PIDs): [${filaPids.join(', ')}]`);
+
+      // Pega o tempo de espera e de execução do processo
+      this.duracao += processoAtual.getDuracao();
+      this.espera += Math.abs(this.duracao - processoAtual.getDuracao() - processoAtual.getIngresso());
+      this.execucao += Math.abs(this.duracao - processoAtual.getIngresso());
+      await processoAtual.executar(processoAtual.getDuracao());
+    }
+    console.log('\n\nTodos os processos foram executados.');
+    console.log('\nLog de execução:');
+    console.log(`Tempo médio de execução: ${this.execucao/this.nProcessos} ms.`);
+    console.log(`Tempo médio de espera: ${this.espera/this.nProcessos} ms.`);
+    console.log(`Número de trocas de contexto: ${this.nProcessos-1}.`);
+    //console.log(`Utilização Média da CPU: ${((this.duracao/)*100)}%.`)
+
+  }
+}
+
+export default ShortestJobFirst;
